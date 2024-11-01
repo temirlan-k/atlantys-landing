d43146c57c5b7054f3ffe9b6ee22a2cc2db64cc9
aidoha <ibrayevaiqyn@gmail.com>
2024-11-01 14:41:48 +0500

    fix:error

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
-import Image from "next/image";
-import React from 'react'
+import React from 'react';
+import ChatBox from '@/components/chat/Chat';
+
+import ContactForm from '@/components/home/contact-form';
+
+import HeroSection from '@/components/home/herosection';
+import PricingSection from '@/components/home/pricing-section';
+import TestimonialsSwiper from '@/components/home/tesnomial-swiper';
+import AbilitySection from '@/components/home/ability-section';
+import AnimatedIconSection from '@/components/home/animated-section';
+import VideoFrame from '@/components/home/video-frame';
+
 export default function Home() {
   return (
-
     <React.Fragment>
+      <HeroSection />
       <AbilitySection />
-
       <VideoFrame />
       <TestimonialsSwiper />
       <PricingSection />
@@ -13,6 +22,5 @@ export default function Home() {
       <ContactForm />
       <ChatBox />
     </React.Fragment>
-
   );
 }
